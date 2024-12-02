f7fa064f0470c1a8aacc73c4ab065fbe5d6740ab
Jasson Ramos <jassonramosdovale@gmail.com>
2024-12-02 15:50:24 -0300

    Commit comum

--- app/src/main/java/com/example/beatfranticallyidle/MainActivity.kt
+++ app/src/main/java/com/example/beatfranticallyidle/MainActivity.kt
@@ -4,16 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.beatfranticallyidle.screen.AppIdle
-import com.example.beatfranticallyidle.screen.BottomAppBar
-import com.example.beatfranticallyidle.screen.TopAppBar
 import com.example.beatfranticallyidle.ui.theme.BeatFranticallyIdleTheme
 
 class MainActivity : ComponentActivity() {
